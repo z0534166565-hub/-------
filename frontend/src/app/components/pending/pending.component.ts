4d8e0a4995b82d8abc99e5284b4d97da0cbe83a7
z0534166565-hub <z0534166565@gmail.com>
2026-09-08 17:23:29 +0300

    Delete frontend directory

--- frontend/src/app/components/pending/pending.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-pending',
-  standalone: true,
-  templateUrl: './pending.component.html',
-})
-export class PendingComponent {}
--- frontend/src/app/models/response-result.model.ts
+++
@@ -1,3 +0,0 @@
-export interface ResponseResult {
-  success: boolean;
-}
