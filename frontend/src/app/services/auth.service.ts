f08f02324c6d54f580651118bd7d59a34fcd2c56
z0534166565-hub <z0534166565@gmail.com>
2026-09-09 12:18:46 +0300

    Update auth.service.ts

--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -1,8 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { firstValueFrom } from 'rxjs';
+
 import { ResponseResult } from '../models/response-result.model';
 import { User } from '../models/user.model';
+
 interface GoogleAuthValues {
   googleOauthUrl: string;
   googleOauthScope: string;
@@ -13,57 +15,124 @@ interface GoogleAuthValues {
   providedIn: 'root'
 })
 export class AuthService {
+
   public userInfo?: User;
 
   constructor(
-    private _http: HttpClient,
-  ) { }
+    private _http: HttpClient
+  ) {}
 
   async loginWithGoogle() {
-    try {
-      const googleAuthValues: GoogleAuthValues = await firstValueFrom(this._http.get<GoogleAuthValues>('/auth/google'));
-      const state = crypto.randomUUID()
-      const params = new URLSearchParams({
-        client_id: googleAuthValues.googleClientId,
-        redirect_uri: window.location.origin + '/login',
-        scope: googleAuthValues.googleOauthScope,
-        state: state,
-        response_type: 'code',
-        access_type: 'offline',
+
+    const googleAuthValues =
+      await firstValueFrom(
+        this._http.get<GoogleAuthValues>(
+          '/auth/google'
+        )
+      );
+
+    const state =
+      crypto.randomUUID();
+
+    const redirectUri =
+      window.location.origin +
+      '/Updates-from-the-House-of-Elders/login';
+
+    const params =
+      new URLSearchParams({
+        client_id:
+          googleAuthValues.googleClientId,
+
+        redirect_uri:
+          redirectUri,
+
+        scope:
+          googleAuthValues.googleOauthScope,
+
+        state:
+          state,
+
+        response_type:
+          'code',
+
+        access_type:
+          'offline'
       });
 
-      localStorage.setItem('google_oauth_state', state);
-      window.location.href = `${googleAuthValues.googleOauthUrl}?${params.toString()}`;
-    } catch (err: any) {
-      throw err;
-    }
+    localStorage.setItem(
+      'google_oauth_state',
+      state
+    );
+
+    window.location.href =
+      `${googleAuthValues.googleOauthUrl}?${params.toString()}`;
   }
 
-  async login(code: string) {
+  async login(
+    code: string
+  ) {
+
     try {
-      let res = await firstValueFrom(this._http.post<ResponseResult>('/auth/login', { code }));
+
+      const res =
+        await firstValueFrom(
+          this._http.post<ResponseResult>(
+            '/auth/login',
+            {
+              code
+            }
+          )
+        );
+
       return res.success;
-    } catch (err: any) {
-      this.userInfo = undefined;
+
+    } catch (err) {
+
+      this.userInfo =
+        undefined;
+
       throw err;
     }
   }
 
   async logout() {
-    let res = await firstValueFrom(this._http.post<ResponseResult>('/auth/logout', {}));
+
+    const res =
+      await firstValueFrom(
+        this._http.post<ResponseResult>(
+          '/auth/logout',
+          {}
+        )
+      );
+
     if (res.success) {
-      this.userInfo = undefined;
+      this.userInfo =
+        undefined;
     }
+
     return res.success;
   }
 
   async loadUserInfo() {
+
     try {
-      this.userInfo = this.userInfo || await firstValueFrom(this._http.get<User>('/api/user-info'))
-    } catch (err: any) {
-      this.userInfo = undefined;
+
+      this.userInfo =
+        this.userInfo ||
+        await firstValueFrom(
+          this._http.get<User>(
+            '/api/user-info'
+          )
+        );
+
+    } catch (err) {
+
+      this.userInfo =
+        undefined;
+
       throw err;
     }
+
     return this.userInfo;
   }
 }
