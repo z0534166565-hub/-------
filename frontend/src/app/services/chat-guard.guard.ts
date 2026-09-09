2566b73c3403066146065814f1b7a28392146db4
z0534166565-hub <z0534166565@gmail.com>
2026-09-09 16:04:39 +0300

    Update chat-guard.guard.ts

--- frontend/src/app/services/chat-guard.guard.ts
+++ frontend/src/app/services/chat-guard.guard.ts
@@ -1,22 +1,5 @@
-import { inject } from '@angular/core';
-import { CanActivateFn, Router } from '@angular/router';
-import { AuthService } from './auth.service';
+import { CanActivateFn } from '@angular/router';
 
-export const AuthGuard: CanActivateFn = async () => {
-  const router = inject(Router);
-  const authService = inject(AuthService);
-
-  try {
-    const userInfo = await authService.loadUserInfo();
-
-    if (userInfo) {
-      return true;
-    }
-
-    return router.createUrlTree(['/login']);
-
-  } catch (err: any) {
-    authService.userInfo = undefined;
-    return router.createUrlTree(['/login']);
-  }
+export const AuthGuard: CanActivateFn = () => {
+  return true;
 };
