085c55d54d696504a1e0b5c1707e13c1a5159cf0
z0534166565-hub <z0534166565@gmail.com>
2026-09-09 12:18:16 +0300

    Update api.interceptor.ts

--- frontend/src/app/api.interceptor.ts
+++ frontend/src/app/api.interceptor.ts
@@ -1,16 +1,19 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 
-const API_URL = 'https://updates-from-the-house-of-elders.onrender.com';
+const API_URL =
+  'https://updates-from-the-house-of-elders.onrender.com';
 
 export const apiInterceptor: HttpInterceptorFn = (req, next) => {
-  // רק בקשות פנימיות ל־API/Auth
+
   if (
     req.url.startsWith('/api/') ||
     req.url.startsWith('/auth/') ||
     req.url.startsWith('/import/')
   ) {
+
     const apiReq = req.clone({
-      url: API_URL + req.url
+      url: API_URL + req.url,
+      withCredentials: true
     });
 
     return next(apiReq);
