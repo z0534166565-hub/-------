10eefee87be20f35cd92f73ed67c78a5cecea1f9
z0534166565-hub <z0534166565@gmail.com>
2026-09-09 18:34:30 +0300

    Update channel.component.ts

--- frontend/src/app/components/channel/channel.component.ts
+++ frontend/src/app/components/channel/channel.component.ts
@@ -1,7 +1,21 @@
-import { Component, ElementRef, OnInit, Renderer2, RendererStyleFlags2, ViewChild } from '@angular/core';
-import { AdvertisingComponent } from "./advertising/advertising.component";
+import {
+  Component,
+  ElementRef,
+  OnInit,
+  Renderer2,
+  RendererStyleFlags2,
+  ViewChild
+} from '@angular/core';
 
-import { Ad, AdsService } from '../../services/ads.service';
+import { Router } from '@angular/router';
+
+import { AdvertisingComponent } from './advertising/advertising.component';
+
+import {
+  Ad,
+  AdsService
+} from '../../services/ads.service';
+
 import {
   NbButtonModule,
   NbIconModule,
@@ -9,15 +23,21 @@ import {
   NbListModule,
   NbMenuModule,
   NbSidebarModule,
-} from "@nebular/theme";
-import { InputFormComponent } from "./chat/input-form/input-form.component";
-import { AuthService } from "../../services/auth.service";
-import { ChannelHeaderComponent } from "./channel-header/channel-header.component";
-import { ChatComponent } from "./chat/chat.component";
+} from '@nebular/theme';
+
+import { InputFormComponent } from './chat/input-form/input-form.component';
+
+import { AuthService } from '../../services/auth.service';
+
+import { ChannelHeaderComponent } from './channel-header/channel-header.component';
+
+import { ChatComponent } from './chat/chat.component';
+
 import { User } from '../../models/user.model';
 
 @Component({
   selector: 'app-channel',
+
   imports: [
     AdvertisingComponent,
     NbLayoutModule,
@@ -29,7 +49,8 @@ import { User } from '../../models/user.model';
     NbSidebarModule,
     NbListModule,
     ChatComponent
-],
+  ],
+
   templateUrl: './channel.component.html',
   styleUrl: './channel.component.scss'
 })
@@ -37,39 +58,87 @@ export class ChannelComponent implements OnInit {
 
   @ViewChild('inputForm', { static: false })
   set inputForm(element: ElementRef) {
+
     if (element) {
+
       setTimeout(() => {
         this.updateInputBottomOffset();
       }, 0);
+
     }
+
   }
 
   constructor(
     private adsService: AdsService,
     private _authService: AuthService,
     private renderer: Renderer2,
-    private el: ElementRef
-  ) { }
+    private el: ElementRef,
+    private router: Router
+  ) {}
 
-  ad: Ad = { src: '', width: 0 };
+  ad: Ad = {
+    src: '',
+    width: 0
+  };
+
   userInfo?: User;
 
   ngOnInit(): void {
+
     this.adsService.getAds().then(ad => {
       this.ad = ad;
     });
-    this._authService.loadUserInfo().then(res => {
-      this.userInfo = res
-    });
+
+    this._authService
+      .loadUserInfo()
+      .then(res => {
+
+        this.userInfo = res;
+
+      });
+
   }
 
-  onInputHeightChanged() {
+  /**
+   * יציאה מהחשבון
+   */
+  async logout(): Promise<void> {
+
+    await this._authService.logout();
+
+    this.userInfo = undefined;
+
+    await this.router.navigate(
+      ['/login'],
+      {
+        replaceUrl: true
+      }
+    );
+
+  }
+
+  onInputHeightChanged(): void {
+
     this.updateInputBottomOffset();
+
   }
 
-  updateInputBottomOffset() {
-    let inputForm = document.getElementById('inputForm');
-    let h = inputForm?.clientHeight;
-    this.renderer.setStyle(this.el.nativeElement, '--input-height', `${h}px`, RendererStyleFlags2.DashCase);
+  updateInputBottomOffset(): void {
+
+    const inputForm =
+      document.getElementById('inputForm');
+
+    const h =
+      inputForm?.clientHeight;
+
+    this.renderer.setStyle(
+      this.el.nativeElement,
+      '--input-height',
+      `${h}px`,
+      RendererStyleFlags2.DashCase
+    );
+
   }
+
 }
