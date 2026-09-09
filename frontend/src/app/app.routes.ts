828f55889083d58a756671cfbb3574ed583ddb2b
z0534166565-hub <z0534166565@gmail.com>
2026-09-09 11:47:47 +0300

    Update app.routes.ts

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,14 +1,17 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { ChannelComponent } from './components/channel/channel.component';
-import { AuthGuard } from "./services/chat-guard.guard";
+import { AuthGuard } from './services/chat-guard.guard';
 
 export const routes: Routes = [
-  { path: 'login', component: LoginComponent },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
   {
     path: '',
     component: ChannelComponent,
-    canActivate: [AuthGuard],
+    canActivate: [AuthGuard]
   },
   {
     path: '**',
